a594ecdc99bd07d9e1cee6a7c7e1e54592fc406b
DhananjaiH <ndhaijaan@gmail.com>
2014-05-19 08:47:48 +0530

    Create repo

---
+++ Assets/Scripts/Webcam.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Webcam : MonoBehaviour 
+{
+
+	public MeshRenderer[] UseWebcamTexture;
+	private WebCamTexture webcamTexture;
+
+	// Use this for initialization
+	void Start () 
+	{
+		WebCamDevice[] devices = WebCamTexture.devices;
+		webcamTexture = new WebCamTexture();
+		print ("devices: " + devices.Length);
+		if (devices.Length > 1)
+		{
+			webcamTexture.deviceName = devices [1].name;
+		}
+		else
+		{
+			webcamTexture.deviceName = devices [0].name;
+		}
+
+		foreach(MeshRenderer r in UseWebcamTexture)
+		{
+			r.material.mainTexture = webcamTexture;
+		}
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+
+	void OnGUI()
+	{
+		if (webcamTexture.isPlaying)
+		{
+			//GUI.Box (new Rect (200, 10, 100, 170), "Menu");
+			if (GUILayout.Button("Pause"))
+			{
+				webcamTexture.Pause();
+			}
+			if (GUILayout.Button("Stop"))
+			{
+				webcamTexture.Stop();
+			}
+		}
+		else
+		{
+			if (GUILayout.Button("Play"))
+			{
+				webcamTexture.Play();
+			}
+		}
+	}
+}
